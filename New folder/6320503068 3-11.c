bad4b5c0fee39f2c1f9ada8b99492e1ed03dce9f
Piya <piya.rat@ku.th>
2021-02-04 16:17:51 +0700

    3-11

--- New folder/6320503068 3-11.c
+++ New folder/6320503068 3-11.c
@@ -3,7 +3,7 @@
     void main()
     {
         unsigned long long int N;
-        scanf("%d",&N);
+        scanf("%llu",&N);
         if ( N>=0 && N<pow(10,1000000) )
         {
             printf("%d %d",N%3 ,N%11);
