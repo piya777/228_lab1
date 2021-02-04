ee68af942c3d36f82fb3a2add4e0109a9e02ea62
Piya <piya.rat@ku.th>
2021-02-04 18:12:33 +0700

    Update 6320503068 Mutant.c

--- New folder/6320503068 Mutant.c
+++ New folder/6320503068 Mutant.c
@@ -6,7 +6,7 @@
         fflush (stdin);
         char first,finish;
         scanf("%c",&first);
-        if( n>0 && n<1000 && m>0 && m <1000 & t>1 && t<1000000000 && n<t && m<t)
+        if( n>0 && n<1000 && m>0 && m <1000 && t>1 && t<1000000000 && n<t && m<t)
         {
             for( i=1 ; i<=t ; i++ )
             {
