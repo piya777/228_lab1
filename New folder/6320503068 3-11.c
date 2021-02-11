56f9c381de83933022c8ed15bc7df93824be7b7f
Piya <piya.rat@ku.th>
2021-02-11 14:52:25 +0700

    Initial commit

--- New folder/6320503068 3-11.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-    void main()
-    {
-        unsigned long long int N ,mthree ,mele;
-        scanf("%llu",&N);
-        mthree=N%3;
-        mele=N%11;
-        printf("%llu %llu",mthree ,mele);
-    }
--- New folder/6320503068 X2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-    void main()
-    {
-        long int x1,x2,s;
-        scanf("%ld %ld",&x1 ,&s);
-        x2=(2*s)-x1;
-        printf("%ld",x2);
-    }
